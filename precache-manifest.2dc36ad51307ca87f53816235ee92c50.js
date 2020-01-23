86f889ee181c41a5b566b4fb48c1c0cd3f2a365d
santistebanc <santistebanc94@gmail.com>
2020-01-24 00:59:50 +0100

    Updates

---
+++ precache-manifest.2dc36ad51307ca87f53816235ee92c50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd748e44be70b69d437154bdab03adee",
+    "url": "/searcher/2f7c8977b5f2f1f2c48a.worker.js"
+  },
+  {
+    "revision": "1f3fef76d766a286d0da7096c0fad708",
+    "url": "/searcher/7a8167aa2d3599d0573c.worker.js"
+  },
+  {
+    "revision": "07549a7eb74f69ca4217ed7a3ca3f5f8",
+    "url": "/searcher/index.html"
+  },
+  {
+    "revision": "ad5af5a9828e9157cead",
+    "url": "/searcher/static/js/2.20be38fa.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/searcher/static/js/2.20be38fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7c4ecb3b7d16359aeca",
+    "url": "/searcher/static/js/main.0eaf5a08.chunk.js"
+  },
+  {
+    "revision": "d0e879981f22f22c2961",
+    "url": "/searcher/static/js/runtime-main.5b0f6dc8.js"
+  }
+]);
